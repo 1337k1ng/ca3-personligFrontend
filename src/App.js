74ca7f3899bd7123c0b7b3f642517cbefad26c83
BenjaminIglesias <cph-bc106@cphbusiness.dk>
2020-11-10 13:19:12 +0100

    Tilføjet Login/Welcome Page(Skabelon), Mangler filtering på JWT TOKEN ROLE

--- src/App.js
+++ src/App.js
@@ -1,25 +1,145 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState,useEffect } from "react"
+import facade from "./apiFacade";
+import {
+  Switch,
+  Route,
+  Link,
+  NavLink,
+  useRouteMatch,
+  Prompt
+} from "react-router-dom";
 
-function App() {
+
+
+
+function Header(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul className="header">
+            <li>
+            <NavLink exact activeClassName="selected" to="/">Home</NavLink>
+            </li>
+            <li>
+            <NavLink  activeClassName="selected" to="/LoginPage">Login</NavLink>
+            </li>
+            
+    </ul>
+   </div>
+  )  
+  }
+  
+  function Home(){
+    return (
+      <div>
+        <h1>This is the Welcome Page</h1>
+      </div>
+    )
+  } 
+
+
+
+  function LoginPage(){
+    const [loggedIn, setLoggedIn] = useState(false)   
+    const [loggedInError, setLoggedInError] = useState("")
+ 
+    const logout = () => { facade.logout()
+      setLoggedIn(false) } 
+    const login = (user, pass) => { facade.login(user,pass)
+      .then(res =>setLoggedIn(true)).catch(err => err.fullError).then(data => setLoggedInError(data));} 
+   
+    
+if(loggedInError){
+  return (
+    <div>
+    <LogIn login={login} />
+  <h3>{loggedInError.message}</h3>
+  </div>
+    )
+  }
+
+    return (
+      <div>
+      {!loggedIn ? (<LogIn login={login} />) :
+        (<div>
+          <LoggedIn />
+          <button onClick={logout}>Logout</button>
+        </div>)}
+</div>
+    )}
+   
+
+function LogIn({ login }) {
+  const init = { username: "", password: "" };
+  const [loginCredentials, setLoginCredentials] = useState(init);
+ 
+  
+  const performLogin = (evt) => {
+    evt.preventDefault();
+    login(loginCredentials.username, loginCredentials.password);
+  }
+  const onChange = (evt) => {
+    setLoginCredentials({ ...loginCredentials,[evt.target.id]: evt.target.value })
+  }
+ 
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onChange={onChange} >
+        <input placeholder="User Name" id="username" />
+        <input type="password" placeholder="Password" id="password" />
+        <button onClick={performLogin}>Login</button>
+      </form>
     </div>
-  );
+  )
+ 
+}
+function LoggedIn() {
+  const [dataFromServer, setDataFromServer] = useState("")
+  const [errorFromServer, setErrorFromServer] = useState("")
+  
+  useEffect(() => { facade.fetchData().then(data=> setDataFromServer(data.msg)) 
+    .catch(err  => err.fullError ).then(facade.fetchDataAdmin().then(data=> setErrorFromServer(data.msg))) 
+    ;       }, [])
+  
+
+
+if(errorFromServer !== ""){
+  console.log(errorFromServer)
+  return(
+    <div>
+  <h3>{errorFromServer}</h3>
+  <h3>Role:Admin</h3>
+  </div>
+  
+  )
 }
 
+
+  return (
+    <div>
+      <h2>Data Received from server</h2>
+      <h3>{dataFromServer}</h3>
+      <h3>Role: User</h3>
+    </div>
+  )
+ 
+}
+ 
+function App() { 
+  return (
+  
+    <div>
+        <Header/>
+        <Switch>
+    <Route exact path="/">
+      <Home />
+    </Route>
+    <Route exact path="/LoginPage">
+      <LoginPage />
+    </Route>
+    </Switch> 
+    </div>
+  )
+ 
+}
 export default App;
